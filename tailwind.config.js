77fa4730aca12eaf015b2f01320b9da660f67f62
Reyhan Andrea Firdaus <reyhananf7@gmail.com>
2025-12-07 18:13:07 +0700

    feature/TMA-376 Improve UI and support change language from form assessment

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import preset from './vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './vendor/andreia/filament-nord-theme/resources/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+            },
+        },
+    },
+}
